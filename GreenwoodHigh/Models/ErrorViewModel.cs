78e6f34d30bc46e4b21d367426229674e26e08b8
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2025-01-11 04:48:12 +0530

    Add project files.

---
+++ GreenwoodHigh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenwoodHigh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
